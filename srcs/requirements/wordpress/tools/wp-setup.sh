ea6122a868e6cf05ab43b18a93851418f7455fb0
user <user@debian.deb>
2024-02-25 03:32:56 +0100

    Fixe Bug: "wp-config.mustache is not a file in phar" {rename wp to wp-cli.phar}

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-service php-fpm7.4 start
-wp core download --path=/var/www/html --allow-root
+service php7.4-fpm start
+wp-cli.phar core download --path=/var/www/html --allow-root
 
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbhost=mariadb \
+wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbhost=mariadb \
        	--dbpass=$DB_USER_PASSWD --path=/var/www/html --allow-root
 
-wp core install --url=$DOMAIN_NAME --title=Inception \
+wp-cli.phar core install --url=$DOMAIN_NAME --title=Inception \
        	--admin_user=$DB_USER --admin_password=$DB_USER_PASSWD \
        	--admin_email=$USER_EMAIL --path=/var/www/html --allow-root
 
-wp user create $DB_STD_USER $DB_STD_EMAIL --user_pass=$DB_STD_PASSWD \
+wp-cli.phar user create $DB_STD_USER $DB_STD_EMAIL --user_pass=$DB_STD_PASSWD \
        	--role=author --path=/var/www/html --allow-root
 
-service php-fpm7.4 stop
+service php7.4-fpm stop
 
 php-fpm7.4 -F
